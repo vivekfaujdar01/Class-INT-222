544e2f9a0af7adc2dbf85b4ee8f58413564eeae6
Vivek <152777809+vivekfaujdar01@users.noreply.github.com>
2025-11-10 11:05:53 +0530

    make server using http

---
+++ core_modules/createServer.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+//create a server
+
+// const server = http.createServer((req, res)=>{
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World\n');
+// })
+
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Server is running at url http://localhost:3000/\n');
+    }
+    else if(req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('This is the about page at url http://localhost:3000/about.\n');
+    }
+    else{
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 Not Found\n');
+    }
+
+})
+
+// server listens on port 3000
+server.listen(3000, ()=>{
+    console.log('Server running at http://localhost:3000/');
+});
