e580cbe86def2d9d2cbb5e3e8e6e7e53527dcccf
Vivek <152777809+vivekfaujdar01@users.noreply.github.com>
2025-11-12 09:13:54 +0530

    create server using http

--- core_modules/createServer.js
+++ core_modules/createServer.js
@@ -7,23 +7,42 @@ const http = require('http');
 //     res.end('Hello World\n');
 // })
 
-const server = http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Server is running at url http://localhost:3000/\n');
-    }
-    else if(req.url === '/about'){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('This is the about page at url http://localhost:3000/about.\n');
-    }
-    else{
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 Not Found\n');
-    }
+// const server = http.createServer((req, res)=>{
+//     if(req.url === '/'){
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end('Server is running at url http://localhost:3000/\n');
+//     }
+//     else if(req.url === '/about'){
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end('This is the about page at url http://localhost:3000/about.\n');
+//     }
+//     else if(req.url === '/contact'){
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end('This is the contact page at url http://localhost:3000/contact.\n');
+//     }
+//     else{
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.end('404 Not Found\n');
+//     }
 
-})
+// })
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    if(req.url === '/student'){
+        const student = {
+            name: "John Doe",
+            age: 21,
+            course: "Computer Science"
+        }; 
+        res.end(JSON.stringify(student));
+    }
+});
 
 // server listens on port 3000
 server.listen(3000, ()=>{
     console.log('Server running at http://localhost:3000/');
 });
+
+// create http server that handles different routes like / , /about and /contact 
